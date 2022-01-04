5fc7d91bce82c6e72db095039bb824b73e99c0fa
“amitchaulagain” <achaulagain123@gmail.com>
2022-01-04 07:03:47 +0545

    prepared student form

---
+++ app/src/main/java/com/example/testapp/ui/login/StudentActivity.java
@@ -0,0 +1,21 @@
+package com.example.testapp.ui.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testapp.R;
+import com.example.testapp.databinding.ActivityLoginBinding;
+
+public class StudentActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_student);
+
+    }
+}
